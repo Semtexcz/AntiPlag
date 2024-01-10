6fbe1d03c442ac30c5ececb09cc63ff739ea5062
Semtex <kopecky.d@gmail.com>
2024-01-10 09:44:19 +0100

    First working version

---
+++ antiplag/github.py
@@ -0,0 +1,130 @@
+import csv
+import os
+from functools import cached_property
+from pprint import pprint
+from typing import Dict
+
+import requests
+
+
+class GitHubClassroom:
+    def __init__(self, token: str):
+        """
+        Initialize the GitHubClassroom object.
+
+        :param token: A string representing the authentication token for GitHub API access.
+        """
+        self._classrooms = []
+        self.token = token
+        self.headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {token}",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+
+    def get_response_json(self, url: str):
+        """
+        Sends a GET request to the specified URL and returns the JSON response.
+
+        :param url: A string representing the URL to which the GET request is to be sent.
+        :return: A dictionary containing the JSON response from the request.
+        """
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    @cached_property
+    def classrooms(self):
+        """
+        Lazily loads and returns a list of classrooms.
+        If not already loaded, it fetches the classroom data from the GitHub API.
+
+        :return: A list of classroom data.
+        """
+        url = "https://api.github.com/classrooms"
+
+        if not self._classrooms:
+            self._classrooms = self.get_response_json(url)
+        return self._classrooms
+
+    def get_classroom_id_from_name(self, classroom_name: str) -> int:
+        """
+        Retrieves the ID of a classroom given its name.
+
+        :param classroom_name: A string representing the name of the classroom.
+        :return: An integer representing the ID of the classroom.
+        """
+        for classroom in self.classrooms:
+            if classroom["name"] == classroom_name:
+                return classroom["id"]
+
+    def get_classroom(self, classroom_id: int | str) -> list:
+        """
+        Fetches and returns the details of a classroom by its ID.
+
+        :param classroom_id: An integer or string representing the ID of the classroom.
+        :return: A list containing the details of the classroom.
+        """
+        url = f"https://api.github.com/classrooms/{classroom_id}"
+        return self.get_response_json(url)
+
+    def get_assigments_for_classroom(self, classroom_id: int | str):
+        """
+        Retrieves a list of assignments for a given classroom.
+
+        :param classroom_id: An integer or string representing the ID of the classroom.
+        :return: A list of assignments for the specified classroom.
+        """
+        url = f"https://api.github.com/classrooms/{classroom_id}/assignments"
+        return self.get_response_json(url)
+
+    def get_accepted_assignments_for_classroom(self, assignment_id: int | str) -> list:
+        """
+        Fetches a list of accepted assignments for a given assignment ID.
+
+        :param assignment_id: An integer or string representing the ID of the assignment.
+        :return: A list containing the accepted assignments.
+        """
+        url = f"https://api.github.com/assignments/{assignment_id}/accepted_assignments"
+        return self.get_response_json(url)
+
+    def get_nick_assigment_repo_dictionary(self, assignment_id: int | str) -> dict[str: str]:
+        accepted_assignments = self.get_accepted_assignments_for_classroom(assignment_id)
+        nick_repo_dictionary = {}
+        for student_assignment in accepted_assignments:
+            nick_repo_dictionary[student_assignment["students"][0]["login"]] = student_assignment["repository"][
+                "html_url"]
+        return nick_repo_dictionary
+
+    def get_nick_name_dict(self, path: str):
+        with open(path, "r") as f:
+            reader = csv.reader(f)
+            nick_name_dict = {}
+            for i, row in enumerate(reader):
+                if i == 1:
+                    continue
+                nick_name_dict[row[1]] = row[0]
+        return nick_name_dict
+
+    def get_name_assignment_repo_dictionary(self, assignment_id: int | str, classroom_roster_path: str) -> dict[str: str]:
+        nick_name_dict = self.get_nick_name_dict(classroom_roster_path)
+        nick_assigment_repo_dictionary = self.get_nick_assigment_repo_dictionary(assignment_id)
+        name_assignment_repo_dict = {}
+        for k, v in nick_assigment_repo_dictionary.items():
+            if nick_name_dict.get(k):
+                name_assignment_repo_dict[nick_name_dict[k]] = v
+            else:
+                name_assignment_repo_dict[k] = v
+        return name_assignment_repo_dict
+
+
+if __name__ == '__main__':
+    token = os.environ.get("GITHUB_TOKEN")
+    g = GitHubClassroom(token)
+    classrooms = g.classrooms
+    f4 = g.get_classroom_id_from_name('F4 - IFM - Season Two')
+    f4_classroom = g.get_classroom(f4)
+    assignments_f4 = g.get_assigments_for_classroom(f4)
+    # print(assignments_f4)
+    # nick_repo_dictionary = g.get_nick_assigment_repo_dictionary(520813)
+    name_repo_dictionary = g.get_name_assignment_repo_dictionary(520813, "/media/Data/Drive/Projekty/Pracovní/Github Classroom/AntiPlag/data/188826_roster.csv")
+    pprint(name_repo_dictionary)
---
+++ antiplag/repositories.py
@@ -0,0 +1,220 @@
+import json
+import os
+import re
+import subprocess
+import tempfile
+from pprint import pprint
+
+import networkx as nx
+import plotly.graph_objects as go
+from esprima import esprima
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class PlagDetector:
+    def __init__(self):
+        self.files_dict = {}
+        self.repos = {}
+        self._similarity_threshold = 0.85
+
+    @property
+    def similarity_threshold(self):
+        return self._similarity_threshold
+
+    @similarity_threshold.setter
+    def similarity_threshold(self, threshold):
+        self._similarity_threshold = threshold
+
+    def clone_github_repos(self, user_repos):
+        # Slovník pro ukládání cest ke staženým repozitářům
+        cloned_repos = {}
+
+        # Vytvoření dočasné složky
+        temp_dir = tempfile.mkdtemp()
+
+        for user, repo_url in user_repos.items():
+            # Získání názvu repozitáře z URL
+            ssh_link = self.get_ssh_link_from_url(repo_url)
+
+            repo_name = repo_url.split('/')[-1]
+
+            # Cesta k repozitáři ve dočasné složce
+            repo_path = os.path.join(temp_dir, repo_name)
+
+            # Stažení repozitáře
+            subprocess.run(['git', 'clone', ssh_link, repo_path])
+
+            # Přidání cesty do slovníku
+            cloned_repos[user] = repo_path
+
+        self.repos = cloned_repos
+
+    def get_ssh_link_from_url(self, url):
+        url_array = url.split("/")
+        return f"git@github.com:{url_array[-2]}/{url_array[-1]}.git"
+
+    def read_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    def remove_comments_and_empty_lines(self, js_code):
+        # Odstranění víceřádkových komentářů
+        js_code = re.sub(r'/\*.*?\*/', '', js_code, flags=re.DOTALL)
+
+        # Odstranění jednořádkových komentářů a prázdných řádků
+        lines = js_code.split('\n')
+        cleaned_lines = [line for line in lines if not re.match(r'^\s*(//.*)?$', line)]
+
+        return '\n'.join(cleaned_lines)
+
+    def compare_files(self):
+        texts = []
+        nicknames = []
+
+        for nick, file_path in self.files_dict.items():
+            if os.path.exists(file_path):
+                texts.append(self.remove_comments_and_empty_lines(self.read_file(file_path)))
+                nicknames.append(nick)
+            else:
+                print(f"Warning: File not found for {nick} at {file_path}")
+
+        if len(texts) < 2:
+            print("Not enough files to compare.")
+            return
+
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(texts)
+
+        cos_similarities = cosine_similarity(tfidf_matrix)
+        plagiarism_results = []
+
+        for i in range(len(nicknames)):
+            for j in range(i + 1, len(nicknames)):
+                if cos_similarities[i, j] > self._similarity_threshold:
+                    result = {
+                        "pair": (nicknames[i], nicknames[j]),
+                        "similarity": cos_similarities[i, j]
+                    }
+                    plagiarism_results.append(result)
+                    # threshold for similarity
+                    print(
+                        f"Potential plagiarism: {nicknames[i]} and {nicknames[j]} (similarity: {cos_similarities[i, j]})")
+        sorted_results = sorted(plagiarism_results, key=lambda x: x['similarity'], reverse=True)
+        return sorted_results
+
+    def create_files_dict(self, path):
+        files_dict = {}
+        for user, repo in self.repos.items():
+            full_path = os.path.join(repo, path)
+            files_dict[user] = full_path
+        self.files_dict = files_dict
+
+    def create_collaboration_graph(self, plagiarism_results):
+        G = nx.Graph()
+
+        # Přidání uzlů a hran do grafu
+        for result in plagiarism_results:
+            user1, user2 = result['pair']
+            similarity = result['similarity']
+            G.add_edge(user1, user2, weight=similarity)
+
+        # Výpočet pozic uzlů
+        pos = nx.spring_layout(G)
+
+        # Vytvoření hran a uzlů pro Plotly graf
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.extend([x0, x1, None])
+            edge_y.extend([y0, y1, None])
+
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        for node in G.nodes():
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=True,
+                colorscale='YlGnBu',
+                size=10,
+                color=list(dict(G.degree()).values()),
+                colorbar=dict(
+                    thickness=15,
+                    title='Počet spojení',
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=2))
+
+        # Přidání textu k uzlům (názvy uživatelů)
+        node_text = []
+        for node in G.nodes():
+            node_text.append(node)
+
+        node_trace.text = node_text
+
+        # Vytvoření grafu
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            title='Interaktivní graf spolupráce mezi uživateli',
+                            titlefont_size=16,
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=20, l=5, r=5, t=40),
+                            annotations=[dict(
+                                text="Pythonová vizualizace s Plotly",
+                                showarrow=False,
+                                xref="paper", yref="paper",
+                                x=0.005, y=-0.002)],
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+
+        fig.show()
+
+
+if __name__ == '__main__':
+    # Příklad použití
+    repo_urls = {'Eliška Semorádová': 'https://github.com/1-IT-Gymnazium/8-loops-Awyyn',
+                 'Filip Janoušek': 'https://github.com/1-IT-Gymnazium/8-loops-FilipnotfounD',
+                 'Filip Černý': 'https://github.com/1-IT-Gymnazium/8-loops-FilipCer',
+                 'Honza-Hoff': 'https://github.com/1-IT-Gymnazium/8-loops-Honza-Hoff',
+                 'Jakub Lehar': 'https://github.com/1-IT-Gymnazium/8-loops-notkorasek',
+                 'Jakub Oškera': 'https://github.com/1-IT-Gymnazium/8-loops-oskyc',
+                 'Janek Hujer': 'https://github.com/1-IT-Gymnazium/8-loops-WestrCZ',
+                 'Jáchym Ježek': 'https://github.com/1-IT-Gymnazium/8-loops-JJJezek',
+                 'Lukáš Otáhal': 'https://github.com/1-IT-Gymnazium/8-loops-Otahlu',
+                 'Mariana Lipková': 'https://github.com/1-IT-Gymnazium/8-loops-Amyk22',
+                 'Matyáš Jan Diviš': 'https://github.com/1-IT-Gymnazium/8-loops-mejtxd',
+                 'Matěj Fibiger': 'https://github.com/1-IT-Gymnazium/8-loops-Matejop',
+                 'Matěj Hladeček': 'https://github.com/1-IT-Gymnazium/8-loops-Chnapak',
+                 'Maxime Gabarre': 'https://github.com/1-IT-Gymnazium/8-loops-gameprotoCZECH',
+                 'Mykola Ivanko': 'https://github.com/1-IT-Gymnazium/8-loops-Stormmm4128',
+                 'Nicol Rozumová': 'https://github.com/1-IT-Gymnazium/8-loops-nikuskaaa',
+                 'Sarah Štěpánová': 'https://github.com/1-IT-Gymnazium/8-loops-Sarash-s',
+                 'Sebastian Jirkal': 'https://github.com/1-IT-Gymnazium/8-loops-Sejge',
+                 'Tobiáš Špringr': 'https://github.com/1-IT-Gymnazium/8-loops-Gravitak',
+                 'privacyconsern': 'https://github.com/1-IT-Gymnazium/8-loops-privacyconsern'}
+    detector = PlagDetector()
+
+    detector.clone_github_repos(repo_urls)
+    detector.create_files_dict("tasks.js")
+    pprint(detector.files_dict)
+    plagiarism_results = detector.compare_files()
+    print(json.dumps(plagiarism_results, ensure_ascii=False))
+    detector.create_collaboration_graph(plagiarism_results)
